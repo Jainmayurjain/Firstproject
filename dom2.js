67164030fb9faa637c4d6d39b075f39eb9817f63
Jainmayurjain <jainm8966@gmail.com>
2023-04-22 21:31:03 +0530

    DOM2

---
+++ dom2.js
@@ -0,0 +1,6 @@
+let li =document.getElementsByClassName('list-group-item');
+li[0].innerHTML='<b>Item 1</b>';
+li[1].innerHTML='<b>Item 2</b>'; 
+li[2].innerHTML='<b>Item 3</b>'; 
+li[2].style.backgroundColor='green';
+li[3].innerHTML='<b>Item 4</b>'; 
