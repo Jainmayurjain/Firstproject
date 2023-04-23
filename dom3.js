4a7a15825721fc664c0baa785e47c1bc9efe9ef0
Jainmayurjain <jainm8966@gmail.com>
2023-04-23 12:51:53 +0530

    Dom3

---
+++ dom3.js
@@ -0,0 +1,5 @@
+let li =document.getElementsByTagName('li');
+li[0].innerHTML='<b>Item 1</b>';
+li[2].style.backgroundColor='green';
+li[4].style.backgroundColor='yellow';
+li[4].style.fontWeight='bold'
